70538dfb958e4cb927156672e85f4f5105a08978
Van Hoang <gmlvan99@gmail.com>
2021-05-19 23:33:08 +0700

    fix

--- blackjack-client/models/User.py
+++ blackjack-client/models/User.py
@@ -4,8 +4,4 @@ class user():
         self.password = password
         self.balance = float(balance)
         self.card_owned = 0
-        
-    def __init__(self, username):
-        self.username = username
-        self.card_owned = 0
-        
+    
