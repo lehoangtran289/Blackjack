ce07b9cb09c7799b5ff3e98f02ad4289b91ef107
Van Hoang <gmlvan99@gmail.com>
2021-06-26 11:35:55 +0700

    add token pop up dialog

--- blackjack-client/view/AddPage.py
+++ blackjack-client/view/AddPage.py
@@ -40,18 +40,28 @@ class addPage(QtWidgets.QWidget):
             QtWidgets.QMessageBox.about(self, 'Invalid information', 'CreditCard and Amount field must only contain number!')
             return
 
-        request = 'ADD ' + self.user.username + ' ' + credit_card_number + ' ' + amount
+        request = 'CARDRQ ' + credit_card_number + " " + self.user.username
         response = self.connection.send_request(request)
         header = self.connection.get_header(response)
-        message = self.connection.get_message(response)
-        
-        if header == 'ADDSUCCESS':
-            _, balance = message.split(' ')
-            self.user.balance = float(balance)
-            QtWidgets.QMessageBox.about(self, 'Add Successful', 'Successful')
-            self.back()
-        else:
-            QtWidgets.QMessageBox.about(self, 'Add Failed', message)
+        if header == "RQFAIL":
+            QtWidgets.QMessageBox.about(self, 'Invalid card', self.connection.get_message(response))
+            return
+
+        token, ok = QtWidgets.QInputDialog.getText(self, "Add/Withdraw token", "Enter token which was sent to your email")
+        if ok:
+            token = str(token)
+            request = 'ADD ' + self.user.username + ' ' + credit_card_number + ' ' + token + ' ' + amount
+            response = self.connection.send_request(request)
+            header = self.connection.get_header(response)
+            message = self.connection.get_message(response)
+            
+            if header == 'ADDSUCCESS':
+                _, balance = message.split(' ')
+                self.user.balance = float(balance)
+                QtWidgets.QMessageBox.about(self, 'Add Successful', 'Successful')
+                self.back()
+            else:
+                QtWidgets.QMessageBox.about(self, 'Add Failed', message)
 
     def back(self): 
         self.home_page = HomePage.homePage(self.user, self.connection, self.pos().x(), self.pos().y() + 30)
--- blackjack-client/view/WithdrawPage.py
+++ blackjack-client/view/WithdrawPage.py
@@ -39,19 +39,28 @@ class withdrawPage(QtWidgets.QWidget):
             QtWidgets.QMessageBox.about(self, 'Invalid information', 'CreditCard and Amount field must only contain number!')
             return
 
-        request = 'WDR ' + self.user.username + ' ' + credit_card_number + ' ' + amount
+        request = 'CARDRQ ' + credit_card_number + " " + self.user.username
         response = self.connection.send_request(request)
-        print('recieved: ' + response)
         header = self.connection.get_header(response)
-        message = self.connection.get_message(response)
+        if header == "RQFAIL":
+            QtWidgets.QMessageBox.about(self, 'Invalid card', self.connection.get_message(response))
+            return
 
-        if header == 'WDRSUCCESS':
-            _, balance = message.split(' ')
-            self.user.balance = float(balance)
-            QtWidgets.QMessageBox.about(self, 'Add Successful', 'Successful')
-            self.back()
-        else:
-            QtWidgets.QMessageBox.about(self, 'Add Failed', message)
+        token, ok = QtWidgets.QInputDialog.getText(self, "Add/Withdraw token", "Enter token which was sent to your email")
+        if ok:
+            token = str(token)
+            request = 'WDR ' + self.user.username + ' ' + credit_card_number + ' ' + token + ' ' + amount
+            response = self.connection.send_request(request)
+            header = self.connection.get_header(response)
+            message = self.connection.get_message(response)
+
+            if header == 'WDRSUCCESS':
+                _, balance = message.split(' ')
+                self.user.balance = float(balance)
+                QtWidgets.QMessageBox.about(self, 'Add Successful', 'Successful')
+                self.back()
+            else:
+                QtWidgets.QMessageBox.about(self, 'Add Failed', message)
 
     def back(self): 
         self.home_page = HomePage.homePage(self.user, self.connection, self.pos().x(), self.pos().y() + 30)
